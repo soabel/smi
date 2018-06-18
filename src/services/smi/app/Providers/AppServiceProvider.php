f51101d769f45d3b7e19516a9efb4f2961a33eaf
Alfredo Benaute <alfredo.benaute@hundred.com.pe>
2018-06-18 06:50:34 -0500

    Creacion de estrucutura de repositorio

---
+++ src/services/smi/app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace smi\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //Se agrega para BD MariaDB
+        Schema::defaultStringLength(191);
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
